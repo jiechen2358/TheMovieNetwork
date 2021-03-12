963173a91b9a16aae6e061aa1b78f66d3e95f161
root <root@sp21-cs411-19.cs.illinois.edu>
2021-03-12 01:27:24 -0600

    Add query sample data from neo4j database on main page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flaskext.mysql import MySQL
+from neo4j import GraphDatabase
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -11,6 +12,53 @@ app.config['MYSQL_DATABASE_DB'] = 'the_movie_network'
 app.config['MYSQL_DATABASE_HOST'] = '172.22.152.19'
 mysql.init_app(app)
 
+# neo4j configurations
+DATABASE_USERNAME="neo4j"
+DATABASE_PASSWORD="CodeBunnyz123"
+DATABASE_URL="bolt://localhost"
+
+class SampleDataFromNeo4j:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def read_sample_from_neo4j(self):
+        with self.driver.session() as session:
+            movies = session.read_transaction(self._read_movie_neo4j)
+            actors = session.read_transaction(self._read_actor_neo4j)
+            return_result = []
+            for i in range(len(movies)):
+                return_result.append([movies[i], actors[i]])
+            return return_result
+    
+    @staticmethod
+    def _read_movie_neo4j(tx):
+        result = tx.run("MATCH (n:Movie) RETURN n LIMIT 3")
+        return_data = []
+        for record in result:        
+            return_data.append(record.values())
+        return return_data
+
+    @staticmethod
+    def _read_actor_neo4j(tx):
+        result = tx.run("MATCH (n:Actors) RETURN n LIMIT 3")
+        return_data = []
+        for record in result:
+            return_data.append(record.values())
+        return return_data
+
+    @staticmethod
+    def _read_relation_neo4j(tx):
+        result = tx.run("MATCH p=()-[r:Cast]->() RETURN p LIMIT 2")
+        return_data = []
+        for record in result:
+            return_data.append(record.values())
+        print(return_data)
+        return return_data
+
 @app.route('/')
 def main():
     conn = mysql.connect()
@@ -18,7 +66,11 @@ def main():
     sql = "Select imdb_name_id, name from actors order by rand() LIMIT 10;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    return  render_template("home.html", results=results)
+
+    neo4jdb = SampleDataFromNeo4j(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD)
+    movies_actors = neo4jdb.read_sample_from_neo4j()
+    neo4jdb.close()
+    return  render_template("home.html", results=results, neo4jResults=movies_actors)
 
 @app.route('/showSignUp')
 def showSignUp():

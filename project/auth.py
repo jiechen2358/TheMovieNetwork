2e26793cab7c6119a28ee1c402b64c42fad9034a
Liu <lul8@sp21-cs411-19.cs.illinois.edu>
2021-03-27 23:05:49 -0500

    add to .gitignore, completed signup and logout function

--- project/auth.py
+++ project/auth.py
@@ -10,30 +10,59 @@ def login():
 		# Create variables for easy access
 		username = request.form['username']
 		password = request.form['password']
-		# Check if account exists using MySQL
+		# Check if user exists using MySQL
 		conn = mysql.connect()
 		cursor = conn.cursor()
 		cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password,))
 		# Fetch one record and return result
-		account = cursor.fetchone()
-		# If account exists in accounts table in out database
-		if account:
+		user = cursor.fetchone()
+		# If user exists in our database
+		if user:
 			# Create session data, we can access this data in other routes
 			session['loggedin'] = True
-			session['id'] = account[0]
-			session['username'] = account[1]
+			session['id'] = user[0]
+			session['username'] = user[1]
 			# Redirect to profile page 
 			return redirect(url_for('main.profile'))
 		else:
-			# Account doesnt exist or username/password incorrect
+			# User doesnt exist or username/password incorrect
 			msg = 'Incorrect username/password!'
 	return render_template('signin.html', msg = msg)
 
+def try_create_user(username, password):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+	user = cursor.fetchone()
+	if user:
+		return 'User already exists!'
+	else:
+		cursor.execute('INSERT INTO users VALUES (NULL, %s, %s)', (username, password,))
+		conn.commit()
+		return 'You have successfully registered!'
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-	return render_template('signup.html')
+	# Output message if something goes wrong...
+	msg = ''
+	# Check if "username", "password" POST requests exist (user submitted form)
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+		# Create variables for easy access
+		username = request.form['username']
+		password = request.form['password']
+		msg = try_create_user(username,password)
+		
+
+	elif request.method == 'POST':
+		# Form is empty... (no POST data)
+		msg = 'Please fill out the form!'
+	# Show registration form with message (if any)
+	return render_template('signup.html', msg=msg)
 
 @auth.route('/logout')
 def logout():
-	return 'Logout'
+	# Remove session data, this will log the user out
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
